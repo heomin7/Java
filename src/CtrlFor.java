867a5f7b51389c8691987614c0780a527889e6bd
heomin7 <hedgar0601@google.com>
2017-04-04 11:06:36 +0900

    initial commit

---
+++ src/CtrlFor.java
@@ -0,0 +1,16 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class CtrlFor {
+    public static void main(String[] args){
+        int idx = 0, j, k, sum = 0;
+
+        for(idx = 1; idx <= 100; idx++){
+            sum += idx;
+        }
+
+        System.out.println("sum:" + sum);
+
+
+    }
+}
