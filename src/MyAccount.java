d25b85565097d46a26b492803fc6101d51f4305f
heomin7 <hedgar0601@google.com>
2017-04-04 11:39:14 +0900

    -

---
+++ src/MyAccount.java
@@ -0,0 +1,14 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class MyAccount {
+    public static void main(String[] args){
+        int busFee = 1600;
+        int lunchpay = 7500;
+        int lattepay = 2500;
+
+        int totalbudget = busFee + lunchpay + lattepay;
+
+        System.out.println("totalbudget: " + totalbudget);
+    }
+}
